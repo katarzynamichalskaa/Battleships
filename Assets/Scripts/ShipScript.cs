d10ef4368ad2d5c4839c40b302906e80da42d708
Katarzyna Michalska <92379328+katarzynamichalskaa@users.noreply.github.com>
2023-07-22 17:37:25 +0200

    Update ShipScript.cs

--- Assets/Scripts/ShipScript.cs
+++ Assets/Scripts/ShipScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class ShipScript : MonoBehaviour
@@ -11,20 +12,26 @@ public class ShipScript : MonoBehaviour
         TwoCellsShip,
         ThreeCellsShip,
         FourCellsShip,
-        Unknown
     }
+
     private ShipType type;
     private bool isShipPlaced = false;
     private int ListIndex = 0;
+    private bool isRotated = false;
+    private int Counter = 0;
     [SerializeField] float xOffset;
+    [SerializeField] float xOffsetRotated;
+    [SerializeField] float yOffset;
     [SerializeField] GameManager gameManager;
+    [SerializeField] TileScript selectedTile;
+
 
     void Start()
     {
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
-    public void ChangeColor(GameObject obj, Vector3 position, ShipType type)
+    public void ChangeColor(GameObject obj, ShipType type)
     {
         isShipPlaced = false;
 
@@ -43,19 +50,91 @@ public class ShipScript : MonoBehaviour
             Color customColor = new Color(0.2f, 0.6f, 0.4f, 1.0f);
             StartCoroutine(Blink(obj.GetComponent<Renderer>().material, customColor));
         }
-        else if (type == ShipType.FourCellsShip)
+        else 
         {
             Color customColor = new Color(0.2f, 0.4f, 0.6f, 0.8f);
             StartCoroutine(Blink(obj.GetComponent<Renderer>().material, customColor));
         }
-        else
-        {
-            Debug.Log("Unknown ship");
-        }
     }
-    public void MoveShip(Vector3 position)
+
+    public void MoveShip(Vector3 position, ShipType type)
     {
+
         transform.position = new Vector3(position.x + xOffset, position.y, position.z);
+
+        //add offset if the second and fourth one is rotated
+        if(isRotated && type == ShipType.FourCellsShip)
+        {
+            transform.position = new Vector3(position.x + xOffsetRotated, position.y + yOffset, position.z);
+        }
+
+        if (isRotated && type == ShipType.TwoCellsShip)
+        {
+            transform.position = new Vector3(position.x + xOffsetRotated, position.y + yOffset, position.z);
+        }
+
+        //LOGIC IF SHIPS ARE ROTATED
+        if (isRotated && type == ShipType.FourCellsShip && position.y <= 4.0f && position.y >= 2.5f)
+        {
+            transform.position = new Vector3(position.x, position.y - 1.3f, position.z);
+        }
+        else if (isRotated && type == ShipType.FourCellsShip && position.y <= -3.0f && position.y >= -5.0f)
+        {
+            transform.position = new Vector3(position.x, position.y + 1.3f, position.z);
+        }
+
+        else if (isRotated && type == ShipType.ThreeCellsShip && position.y <= 4.0f && position.y >= 2.5f)
+        {
+            transform.position = new Vector3(position.x, position.y - 1.1f, position.z);
+        }
+        else if (isRotated && type == ShipType.ThreeCellsShip && position.y <= -3.0f && position.y >= -5.0f)
+        {
+            transform.position = new Vector3(position.x, position.y + 1.0f, position.z);
+        }
+
+        else if (isRotated && type == ShipType.TwoCellsShip && position.y <= -3.0f && position.y >= -5.0f)
+        {
+            transform.position = new Vector3(position.x, position.y + 0.55f, position.z);
+        }
+
+        else if (isRotated && type == ShipType.TwoCellsShip && position.y <= 4.0f && position.y >= 2.5f)
+        {
+            transform.position = new Vector3(position.x, position.y - 0.5f, position.z) ;
+        }
+
+
+
+        //LOGIC IF SHIPS AREN'T ROTATED
+        if (!isRotated && type == ShipType.FourCellsShip && position.x <= 7.0f && position.x >= 5.5f)
+        {
+            transform.position = new Vector3(position.x - 1.2f, position.y, position.z);
+        }
+
+        else if (!isRotated && type == ShipType.FourCellsShip && position.x <= -1.0f && position.x >= -2.0f)
+        {
+            transform.position = new Vector3(position.x + 1.2f, position.y, position.z);
+        }
+
+        else if (!isRotated && type == ShipType.ThreeCellsShip && position.x <= 7.0f && position.x >= 5.5f)
+        {
+            transform.position = new Vector3(position.x - 0.9f, position.y, position.z);
+        }
+
+        else if (!isRotated && type == ShipType.ThreeCellsShip && position.x <= -1.0f && position.x >= -2.0f)
+        {
+            transform.position = new Vector3(position.x + 0.9f, position.y, position.z);
+        }
+
+        else if (!isRotated && type == ShipType.ThreeCellsShip && position.x <= -1.0f && position.x >= -2.0f)
+        {
+            transform.position = new Vector3(position.x + 0.6f, position.y, position.z);
+        }
+
+        if (!isRotated && type == ShipType.TwoCellsShip && position.x <= 7.0f && position.x >= 5.5f)
+        {
+            transform.position = new Vector3(position.x - 0.5f, position.y, position.z);
+        }
+
         isShipPlaced = true;
     }
 
@@ -74,13 +153,9 @@ public class ShipScript : MonoBehaviour
         {
             return ShipType.ThreeCellsShip;
         }
-        else if (ship.CompareTag("4cells_ship"))
-        {
-            return ShipType.FourCellsShip;
-        }
         else
         {
-            return ShipType.Unknown;
+            return ShipType.FourCellsShip;
         }
     }
 
@@ -98,29 +173,26 @@ public class ShipScript : MonoBehaviour
         }
     }
 
-    public void RotateShip(int Counter)
+    public void RotateShip()
     {
+        Counter++;
 
-        if(Counter == 2)
+        if (Counter == 2)
         {
             transform.Rotate(Vector3.forward, 90f);
-            transform.Rotate(Vector3.up, 180f);
-        }
-        else if (Counter == 3)
-        {
-            transform.Rotate(Vector3.up, -180f);
-            transform.Rotate(Vector3.forward, 90f);
-        }
-        else if (Counter == 4)
-        {
-            transform.Rotate(Vector3.forward, -90f);
-            transform.Rotate(Vector3.up, 0f);
+            isRotated = false;
+            Counter = 0;
         }
         else
         {
             transform.Rotate(Vector3.forward, -90f);
-
+            isRotated = true;
         }
+    }
+
+    public int ReturnIfRotated()
+    {
+        return Counter + 1;
     }
 
 }

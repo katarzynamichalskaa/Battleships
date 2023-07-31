cbab613e8e5b615ca2a4aafb82289384f8e14592
Katarzyna Michalska <92379328+katarzynamichalskaa@users.noreply.github.com>
2023-07-31 14:42:42 +0200

    Create TileScript.cs

---
+++ Assets/Scripts/TileScript.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class TileScript : MonoBehaviour
+{
+    [SerializeField] GameObject[] ships;
+    [SerializeField] bool isAvailable = true;
+
+    void Update()
+    {
+        CheckCollision();
+    }
+
+    void CheckCollision()
+    {
+        Physics.SyncTransforms();
+
+        foreach (GameObject obj in ships)
+        {    
+            if (Physics2D.OverlapBox(transform.position, transform.localScale, 0f) == obj.GetComponent<Collider2D>())
+            {
+                isAvailable = false;
+                
+            }
+
+            else
+            {
+                isAvailable = true;
+            }
+        }
+    }
+
+    public bool ReturnAvailability()
+    {
+        return isAvailable;
+    }
+
+    public void SetAvailability()
+    {
+        isAvailable = false;
+    }
+    public void ResetAvailability()
+    {
+        isAvailable = false;
+    }
+
+}
